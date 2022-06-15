421219fc3c61ab3db9a514a83ae0eedb179b5a38
guachismingos <gradiel101rojas@gmail.com>
2022-06-15 17:41:31 -0600

    Basic setup configuration and startup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { IonContent, IonPage } from "@ionic/react";
+
+const Home: FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>Home</IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
---
+++ src/pages/Requirements.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { IonPage, IonContent } from "@ionic/react";
+
+const Requirements: FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>Requirements</IonContent>
+    </IonPage>
+  );
+};
+
+export default Requirements;
